eee1af9f8f78a4ceae5720829fd9a54481aab771
Rus <de.mr44@gmail.com>
2021-07-14 10:56:53 +0500

    Initial commit

---
+++ UI Elements/Label.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  UI Elements
+//
+//  Created by ruslan on 14.07.2021.
+//
+
+import UIKit
+
+class Label: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
+
