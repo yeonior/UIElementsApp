c0f59ffd8b6c28bb1149d8076d60941caf1ddb67
Rus <de.mr44@gmail.com>
2021-08-07 22:42:01 +0500

    Updated WKWebView

--- UI Elements/Controllers/WebViewVC.swift
+++ UI Elements/Controllers/WebViewVC.swift
@@ -15,7 +15,6 @@ class WebViewVC: UIViewController, WKNavigationDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        title = "yandex.ru"
         configureWebView()
         
         if let url = URL(string: "https://yandex.ru") {
@@ -33,18 +32,11 @@ class WebViewVC: UIViewController, WKNavigationDelegate {
         myWebView = WKWebView(frame: view.bounds, configuration: configuration)
         myWebView.backgroundColor = .systemBackground
         myWebView.navigationDelegate = self
+        myWebView.allowsBackForwardNavigationGestures = true
         
-        let backImage = UIImage(systemName: "arrow.backward")
-        let forwardImage = UIImage(systemName: "arrow.forward")
-        let backBarButton = UIBarButtonItem(image: backImage, style: .plain, target: self, action: #selector(goBack(_:)))
-        let forwardBarButton = UIBarButtonItem(image: forwardImage, style: .plain, target: self, action: #selector(goForward(_:)))
-        let refreshBarButton = UIBarButtonItem(barButtonSystemItem: .refresh, target: self, action: #selector(refreshPage(_:)))
+        let refreshBarButton = UIBarButtonItem(barButtonSystemItem: .refresh, target: myWebView, action: #selector(myWebView.reload))
         let doneBarButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(doneButtonTapped(_:)))
-        navigationItem.leftBarButtonItems = [backBarButton, forwardBarButton]
-        navigationItem.leftBarButtonItems?.forEach({ button in
-            button.isEnabled = false
-        })
-        navigationItem.leftBarButtonItems?.append(refreshBarButton)
+        navigationItem.leftBarButtonItem = refreshBarButton
         navigationItem.rightBarButtonItem = doneBarButton
         
         view.addSubview(myWebView)
@@ -58,44 +50,11 @@ class WebViewVC: UIViewController, WKNavigationDelegate {
         ])
     }
     
-    @objc private func goBack(_ sender: UIBarButtonItem) {
-        guard let leftBarButtonItems = navigationItem.leftBarButtonItems, sender == leftBarButtonItems[0] else { return }
-        
-        if myWebView.canGoBack {
-            myWebView.goBack()
-        }
-    }
-    
-    @objc private func goForward(_ sender: UIBarButtonItem) {
-        guard let leftBarButtonItems = navigationItem.leftBarButtonItems, sender == leftBarButtonItems[1] else { return }
-        
-        if myWebView.canGoForward {
-            myWebView.goForward()
-        }
-    }
-    
-    @objc private func refreshPage(_ sender: UIBarButtonItem) {
-        guard let leftBarButtonItems = navigationItem.leftBarButtonItems, sender == leftBarButtonItems[2] else { return }
-        myWebView.reload()
-    }
-    
     @objc private func doneButtonTapped(_ sender: UIBarButtonItem) {
-        guard sender == navigationItem.rightBarButtonItem else { return }
         dismiss(animated: true, completion: nil)
     }
     
-    func webView(_ webView: WKWebView, didCommit navigation: WKNavigation!) {
-        guard let leftBatButtonItems = navigationItem.leftBarButtonItems else { return }
-        leftBatButtonItems[0].isEnabled = false
-        leftBatButtonItems[1].isEnabled = false
-    }
-    
     func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
-        guard let leftBatButtonItems = navigationItem.leftBarButtonItems else { return }
-        if webView.canGoBack {
-            leftBatButtonItems[0].isEnabled = true
-        } else if webView.canGoForward {
-            leftBatButtonItems[1].isEnabled = true
-        }
+        title = webView.title
     }
 }
